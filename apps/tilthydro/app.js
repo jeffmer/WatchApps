1b2e9400d80c3c079d652f3129722a23fced5ab8
Gordon Williams <gw@pur3.co.uk>
2021-09-02 13:39:00 +0100

    squash warnings

--- apps/tilthydro/app.js
+++ apps/tilthydro/app.js
@@ -21,14 +21,14 @@ function arrayBufferToHex (arrayBuffer){
 }
 
 var TILT_DEVICES = {
-    'a495bb30c5b14b44b5121370f02d74de': 'Black',
-    'a495bb60c5b14b44b5121370f02d74de': 'Blue',
-    'a495bb20c5b14b44b5121370f02d74de': 'Green',
-    'a495bb50c5b14b44b5121370f02d74de': 'Orange',
-    'a495bb80c5b14b44b5121370f02d74de': 'Pink',
-    'a495bb40c5b14b44b5121370f02d74de': 'Purple',
-    'a495bb10c5b14b44b5121370f02d74de': 'Red',
-    'a495bb70c5b14b44b5121370f02d74de': 'Yellow',
+  'a495bb30c5b14b44b5121370f02d74de': 'Black',
+  'a495bb60c5b14b44b5121370f02d74de': 'Blue',
+  'a495bb20c5b14b44b5121370f02d74de': 'Green',
+  'a495bb50c5b14b44b5121370f02d74de': 'Orange',
+  'a495bb80c5b14b44b5121370f02d74de': 'Pink',
+  'a495bb40c5b14b44b5121370f02d74de': 'Purple',
+  'a495bb10c5b14b44b5121370f02d74de': 'Red',
+  'a495bb70c5b14b44b5121370f02d74de': 'Yellow',
 };
 
 var failures = 0;
