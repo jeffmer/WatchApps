4800085b82d7dd6260f887e96d59fb1063299859
jeffmer <jeffreymagee@gmail.com>
2021-11-13 09:16:11 +0000

    update setui

--- apps/main/setui.js
+++ apps/main/setui.js
@@ -14,23 +14,23 @@ wOS.setUI =function(mode, cb) {
     if (!mode) return;
     else if (mode=="updown") {
         wOS.btnWatches = [
-          setWatch(function() { cb(); }, BTN1, {repeat:1}),
+          setWatch(function() { cb(); }, BTN1, {repeat:1,edge:"falling"}),
         ];
         TC.swipeHandler = d => {if (d==2) cb(-1); else if (d==1)  cb(1);};
         TC.on("swipe", TC.swipeHandler);    }
     else if (mode=="leftright") {
         wOS.btnWatches = [
-          setWatch(function() {cb();}, BTN1, {repeat:1,edge:"rising"}),
+          setWatch(function() {cb();}, BTN1, {repeat:1,edge:"falling"}),
         ];
         TC.swipeHandler = d => {if (d==3) cb(-1); else if (d==4)  cb(1);};
         TC.on("swipe", TC.swipeHandler);
     } else if (mode=="clock") {
         wOS.btnWatches = [
-          setWatch(function() {if (wOS.awake) wOS.showLauncher(); }, BTN1, {repeat:1,edge:"rising"}),
+          setWatch(function() {if (wOS.awake) wOS.showLauncher(); }, BTN1, {repeat:1,edge:"falling"}),
         ];
     } else if (mode=="touch") {
       wOS.btnWatches = [
-        setWatch(function() {if (wOS.awake) load("launch.js"); }, BTN1, {repeat:1,edge:"rising"}),
+        setWatch(function() {if (wOS.awake) load("launch.js"); }, BTN1, {repeat:1,edge:"falling"}),
       ];
       TC.touchHandler = d => {cb(d);};
       TC.on("touch", TC.touchHandler);
