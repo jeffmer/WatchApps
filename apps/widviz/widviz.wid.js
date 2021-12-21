a9c937aabd25f541b38368f73bda14b9d3bc9636
jeffmer <jeffreymagee@gmail.com>
2021-12-21 12:27:17 +0000

    add widviz

---
+++ apps/widviz/widviz.wid.js
@@ -0,0 +1,38 @@
+(() => {
+
+  var saved = null;
+  var hidewid = false;
+  
+  function hide(){
+    if (!Bangle.isLCDOn() || saved) return;
+    saved = [];
+    for (var wd of WIDGETS) {
+      saved.push(wd.draw); 
+      wd.draw=()=>{};
+    }
+    g.setColor(g.theme.bg);
+    g.fillRect(0,0,239,23);
+  }
+  
+  function reveal(){
+    if (!Bangle.isLCDOn() || !saved) return;
+    for (var wd of WIDGETS) wd.draw = saved.shift();
+    Bangle.drawWidgets(); 
+    saved=null;
+  }
+  
+  function draw(){
+    var img = E.toArrayBuffer(atob("GBgBAAAAAAAAAAAAAAAAAH4AAf+AB4HgDgBwHDw4OH4cMOcMYMMGYMMGMOcMOH4cHDw4DgBwB4HgAf+AAH4AAAAAAAAAAAAAAAAA"));
+    g.setColor(0x07ff);
+    g.drawImage(img,this.x,this.y);
+  }
+    
+  WIDGETS["viz"] ={area:"tl", width:24,draw:draw};
+
+  TC.on('touch',(pt)=>{
+    if (pt.x<40 && pt.y<40) {
+      hidewid = !hidewid;
+      if (hidewid) hide(); else reveal();
+    }
+  });    
+})();
