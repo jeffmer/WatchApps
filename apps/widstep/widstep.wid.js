76c22993dfd89d4dfd47e5423ffdeb8dc4f39ec7
jeffmer <jeffreymagee@gmail.com>
2022-03-07 17:46:58 +0000

    update apps.json

--- apps/widstep/widstep.wid.js
+++ apps/widstep/widstep.wid.js
@@ -2,7 +2,10 @@
   
     function draw() {
       g.setColor(g.theme.fg);
-      g.setFont("Vector",16).setFontAlign(0,0).drawString(("000"+E.totalSteps()).substr(-4),this.x+20,this.y+12);
+      if (wOS.settings.steps) 
+        g.setFont("Vector",16).setFontAlign(0,0).drawString(("000"+E.totalSteps()).substr(-4),this.x+20,this.y+12);
+      else
+        g.setFont("Vector",16).setFontAlign(0,0).drawString("    ",this.x+20,this.y+12);
     }
   
     WIDGETS["step"]={area:"tr",width:40,draw:draw};
