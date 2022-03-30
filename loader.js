fe9ad87b3b0e3f542d2a9adeb3ecb994fefd49f8
jeffmer <jeffreymagee@gmail.com>
2022-03-30 10:55:45 +0100

    Revert "add gw32 unbuf"
    This reverts commit 849ac35ab6b5f720ecbfd6bd6acf7dda65eda1ff.

--- loader.js
+++ loader.js
@@ -71,12 +71,6 @@ const MYDEVICEINFO = [ {
     name : "GW32",
     features : ["BLE","BLEHID","NFC","GRAPHICS"],
     img : "img/gw32.jpeg"
-  },
-  {
-    id : "GW32UNBUF",
-    name : "GW32UNBUF",
-    features : ["BLE","BLEHID","NFC","GRAPHICS"],
-    img : "img/gw32.jpeg"
   }
 ];
 
